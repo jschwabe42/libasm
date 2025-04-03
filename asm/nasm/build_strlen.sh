e64aba30f25313869e308da015cbeb4766148756
Jonstep101010 <git@jschwabe.site>
2025-04-03 19:06:23 +0200

    WIP(strlen): check for segfault on null, naming
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- asm/nasm/build_strlen.sh
+++ asm/nasm/build_strlen.sh
@@ -2,4 +2,4 @@
 
 nasm -f macho64 ft_strlen.s -o ft_strlen.o
 cc -Wall -Wextra -Werror -arch x86_64 -c strlen_main.c -o main.o
-cc -Wall -Wextra -Werror -arch x86_64 main.o ft_strlen.o -o test_strlen
+cc -Wall -Wextra -Werror -arch x86_64 main.o ft_strlen.o -o test_strlen.out
--- asm/nasm/strlen_main.c
+++ asm/nasm/strlen_main.c
@@ -1,12 +1,31 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <setjmp.h>
+#include <stdio.h>
 
 extern size_t	ft_strlen(const char *str);
+
+static jmp_buf jump_buffer;
+
+static void signal_handler(int signum) {
+    if (signum == SIGSEGV) {
+        longjmp(jump_buffer, 1);
+    }
+}
 
 int main() {
 	const char	*src = "Hello, world!";
 	const size_t		len = ft_strlen(src);
 	assert(strlen(src) == len);
+	assert(strlen("") == ft_strlen(""));
+
+    signal(SIGSEGV, signal_handler);
+
+    if (setjmp(jump_buffer) == 0) {
+        ft_strlen(NULL);
+        fprintf(stderr, "ft_strlen(NULL) did not segfault!\n");
+        abort();
+    }
 	return (0);
 }
