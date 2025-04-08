6083bb8d91358ec5f0d652ce1ffa5d9658194db7
Jonstep101010 <git@jschwabe.site>
2025-04-08 19:04:33 +0200

    WIP(strcmp): arm64 implementation, fix testing
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- src/inline_arm64.rs
+++ src/inline_arm64.rs
@@ -1,7 +1,10 @@
 #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
 core::arch::global_asm!(include_str!("../asm/arm/strlen.S"));
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+core::arch::global_asm!(include_str!("../asm/arm/strcmp.S"));
 
 #[allow(dead_code)]
 unsafe extern "C" {
-	pub fn strlen(str: *const u8) -> usize;
+	pub fn my_strlen(str: *const u8) -> usize;
+	pub fn my_strcmp(str1: *const u8, str2: *const u8) -> i32;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod inline_arm64;
 #[link(name = "asm", kind = "static")]
 unsafe extern "C" {
 	fn ft_strlen(str: *const u8) -> usize;
+	fn ft_strcmp(str1: *const u8, str2: *const u8) -> i32;
 }
 
 #[rstest::rstest]
@@ -17,7 +18,7 @@ fn test_strlen(#[case] input: &std::ffi::CStr, #[case] expected_len: usize) {
 		#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 		let my_len = crate::ft_strlen(input.as_ptr().cast());
 		#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
-		let my_len = crate::inline_arm64::strlen(input.as_ptr().cast());
+		let my_len = crate::inline_arm64::my_strlen(input.as_ptr().cast());
 		#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
 		eprintln!("using arm64 inlined version!");
 		assert_eq!(libc_len, expected_len, "sanity check");
@@ -35,7 +36,7 @@ fn test_strlen_null_segfaults() {
 	let my_result = execute_in_isolated_process(|| unsafe { crate::ft_strlen(std::ptr::null()) });
 	#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
 	let my_result =
-		execute_in_isolated_process(|| unsafe { crate::inline_arm64::strlen(std::ptr::null()) });
+	execute_in_isolated_process(|| unsafe { crate::inline_arm64::my_strlen(std::ptr::null()) });
 	#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
 	eprintln!("using arm64 inlined version!");
 	for result in [libc_result, my_result] {
@@ -50,5 +51,26 @@ fn test_strlen_null_segfaults() {
 			}),
 			"expected segfault with null input"
 		)
+	}
+}
+
+
+#[rstest::rstest]
+#[case(c"hello", c"")]
+#[case(c"", c"hello")]
+#[case(c"", c"")]
+#[case(c"he", c"h")]
+#[case(c"h", c"he")]
+#[rstest::fixture]
+fn test_strcmp(#[case] input: &std::ffi::CStr, #[case] input2: &std::ffi::CStr) {
+	unsafe {
+		let libc_len = libc::strcmp(input.as_ptr().cast(), input2.as_ptr().cast());
+		#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+		let my_len = crate::ft_strcmp(input.as_ptr().cast(), input2.as_ptr().cast());
+		#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+		let my_len = crate::inline_arm64::my_strcmp(input.as_ptr().cast(), input2.as_ptr().cast());
+		#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+		eprintln!("using arm64 inlined version!");
+		assert_eq!(libc_len, my_len, "comparison to libc strlen failed");
 	}
 }
