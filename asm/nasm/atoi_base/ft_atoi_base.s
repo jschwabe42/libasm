204d4782a6738a310dae2b758fa810396bf8eb52
Jonstep101010 <git@jschwabe.site>
2025-05-02 21:30:12 +0200

    WIP(atoi_base): basic tests passing
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- asm/nasm/atoi_base/ft_atoi_base.s
+++ asm/nasm/atoi_base/ft_atoi_base.s
@@ -83,58 +83,114 @@ check_base:
 	mov rax, 1
 	ret
 
+; rdi, rsi, rdx call with str, base, base_len
+convert:
+	mov rcx, rdi ; save rdi (str)
+	xor r8, r8 ; initialize total (rax busy)
+.loop_convert_outer:
+	movzx rdi, byte [rcx]
+	test rdi, rdi
+	jz .do_return ; end of str!
+	call _my_isspace
+	; if space found (1) -> return 0
+	cmp rax, 1
+	je .ret_zero
+	mov r9, -1 ; digit_value
+	xor r10, r10 ; inner loop counter
+	jmp .loop_inner
+.loop_inner:
+	cmp r10, rdx
+	jge .loop_sanity_check
+	; run loop with comparison, assignment
+	movzx rdi, byte [rcx]
+	movzx r11, byte [rsi + r10]
+	cmp dil, r11b
+	je .inner_base_found
+.inner_base_found:
+	mov r9, r10
+	jmp .loop_sanity_check
+.loop_sanity_check:
+	cmp r9, -1
+	mov rax, r8
+	jne .loop_outer_next
+	ret
+.loop_outer_next:
+	mov rax, r8
+	mul rdx
+	add rax, r9
+	mov r8, rax
+	; calculate result, put back into r8
+	inc rcx
+	jmp .loop_convert_outer
+.ret_zero:
+	xor rax, rax
+	ret
+.do_return:
+	; put total into rax
+	mov rax, r8
+	ret
+
+
 ; rdi: str, rsi: base
 _ft_atoi_base:
+	push rbx
 	push rdi
 	mov rdi, rsi
 	call _ft_strlen
 	cmp rax, 2
 	jl .ret_zero ; base is less than 2 characters
-	push rsi
+	push rsi ; base
+	push rax ; length
 	mov rsi, rax
 	; rdi: base, rsi: length 
 	call check_base
 	test rax, rax
-	jne .ret_zero ; base checks failed
+	jnz .ret_zero ; base checks failed
 	; @todo implement checks: str
 	; skip whitespace prefixes
-	; pop rsi
-	; pop rdi
-	push rax ; length
-	mov rax, [rsp + 8] ; access input (str)
+	mov rbx, [rsp + 16] ; access input (str)
 .skip_ws:
-	movzx rdi, byte [rax] ; byte at current address index
+	movzx rdi, byte [rbx] ; byte at current address index
 	call _my_isspace
 	test rax, rax
 	jz .check_prefix ; zero: no more prefix
-	inc rax
+	inc rbx
 	jmp .skip_ws
 .check_prefix:
 	; @follow-up return on error
 	; @todo implement conversion
 	xor rcx, rcx
-	movzx rdi, byte [rax]
+	movzx rdi, byte [rbx]
 	cmp dil, 0x2D ; '-'
 	jne .check_plus
 	;path A: sure to be negative at this point
 	mov cl, 1 ; sign flag
-	inc rax
+	inc rbx
 .check_plus:
 	;path B: positive or no prefix
 	cmp dil, 0x2B ; '+'
 	jne .start_conversion
-	inc rax
+	inc rbx
 	jmp .start_conversion
 .start_conversion:
-	mov rdi, rax ; str
-	pop rdi ; base_length
+	mov rdi, rbx ; &str[..]
+	pop rdx ; base_length
 	pop rsi ; base
+	pop rax ; dump str from stack
 	push rcx ; sign
 	; call with str, base, base_len
+	call convert
 	; apply sign
+	pop rcx
 	cmp cl, 1
+	je .ret_sign
+	leave
+	ret
 .ret_sign:
 	; called if sign is 1 (cl)
+	neg rax
+	leave
+	ret
 .ret_zero:
 	xor rax, rax
 	ret
