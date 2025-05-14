aa8bc390639493852b473efbca5b14e0abe70cfb
Jonstep101010 <git@jschwabe.site>
2025-05-14 10:51:30 +0200

    chore: allow validating inlined against non-inlined
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- asm/nasm/linked_list/build_linked_list.sh
+++ asm/nasm/linked_list/build_linked_list.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 nasm -Werror -f macho64 ft_linked_list.s -o ft_linked_list.o
+nasm -Werror -f macho64 sort_fn_calls.s -o sort_fn_calls.o
 cc -Wall -Wextra -Werror -arch x86_64 -c linked_list_main.c -o main.o
-cc -Wall -Wextra -Werror -arch x86_64 main.o ft_linked_list.o -o test_linked_list.out
+cc -Wall -Wextra -Werror -arch x86_64 main.o sort_fn_calls.o ft_linked_list.o -o test_linked_list.out
