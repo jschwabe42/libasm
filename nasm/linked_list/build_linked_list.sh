369685a6e1d93057099f873e7405e282224f2d78
Jonstep101010 <git@jschwabe.site>
2025-05-20 17:07:00 +0200

    chore: remove other linked list implementation
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- nasm/linked_list/build_linked_list.sh
+++ nasm/linked_list/build_linked_list.sh
@@ -12,7 +12,5 @@ ARCH=""
 fi
 
 nasm -Werror -g -f ${FORMAT} ft_linked_list.s -o ft_linked_list.o
-# nasm -Werror -f ${FORMAT} sort_fn_calls.s -o sort_fn_calls.o
 clang -Wall -Wextra -Werror -g -fsanitize=address,undefined -fno-omit-frame-pointer ${ARCH} -c linked_list_main.c -o main.o
 clang -Wall -Wextra -Werror -g -fsanitize=address,undefined -fno-omit-frame-pointer ${ARCH} main.o ft_linked_list.o -o test_linked_list.out
-# clang -Wall -Wextra -Werror ${ARCH} main.o sort_fn_calls.o ft_linked_list.o -o test_linked_list.out
